be1184e97feb5c6037ed4a692afd5a864b7f1ccb
MelvicTN <mtnavida@gmail.com>
2024-07-28 14:10:33 -0700

    Updated sw_dev_app.py: imported libraries > load *.csv file from GitHub repository SW_Development_Tool

---
+++ sw_dev_app.py
@@ -0,0 +1,11 @@
+# import libaries
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# load *.csv file into dataframes from my GitHub repository 
+try:
+    vehicles_df = pd.read_csv('vehicles_us.csv')
+except:
+    #vehicles_df = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv')
+    vehicles_df = pd.read_csv('https://github.com/MelvicTN/SW_Development_Tool.git')
