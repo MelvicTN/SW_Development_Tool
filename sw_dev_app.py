d72482a91801b7f2f6da923183f5dc76bf0092c2
MelvicTN <mtnavida@gmail.com>
2024-07-29 15:10:37 -0700

    Updated sw_dev_app.py: code to load *.csv

--- sw_dev_app.py
+++ sw_dev_app.py
@@ -4,8 +4,14 @@ import pandas as pd
 import plotly.express as px
 
 # load *.csv file into dataframes from my GitHub repository 
-try:
-    vehicles_df = pd.read_csv('vehicles_us.csv')
-except:
-    #vehicles_df = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv')
-    vehicles_df = pd.read_csv('https://github.com/MelvicTN/SW_Development_Tool.git')
+#try:
+#    vehicles_df = pd.read_csv('vehicles_us.csv')
+#except:
+#    #vehicles_df = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv')
+#    vehicles_df = pd.read_csv('https://github.com/MelvicTN/SW_Development_Tool.git')
+
+
+# load *.csv file from GitHub repository
+
+
+vehicles_df = pd.read('cleaned_vehicles_us.csv')
