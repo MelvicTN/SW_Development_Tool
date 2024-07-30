fc11e17ba920d1301bf478623bd6746490bfd1ed
MelvicTN <mtnavida@gmail.com>
2024-07-30 07:10:56 -0700

    Updated sw_dev_app.py: clean coding

--- sw_dev_app.py
+++ sw_dev_app.py
@@ -24,54 +24,68 @@ df = pd.read_csv('vehicles_cleaned.csv')
 
 
 
+
 # DATA VISUALIZATION-Histogram-1: Vehicles Sale Days Posted by Condition
-#df = clean_VEH_df
 fig_histogram_1 = px.histogram(df, x="days_listed", nbins=90, color='condition', title='Vehicles Sale Days Posted by Condition', hover_data=['model_year', 'make', 'model_name'])
 
-#fig.show()
 # Display the plot using streamlit
-st.plotly_chart(fig_histogram_1)
 st.title('Histogram-1: Vehicles Sale Days Posted by Condition')
+st.plotly_chart(fig_histogram_1)
+
+
+
+# checkbox task/create
+show_histogram = st.checkbox('Show Histogram')
+
+# x = x-axos, labels = checkbox options, yaxis
+if show_histogram:
+    fig_histogram_1 = px.histogram(df, x='days_listed', title='Vehicle Condition', labels={'conditions':'Car Type'})
+    #fig.update_layout(yaxis_title="Amount in Stock")
+    st.plotly_chart(fig_histogram_1)
+
 
 
 
 
 # DATA VISUALIZATION-Histogram: Manufactuer Breakdown by Vehicle Type
-#df = clean_VEH_df
 fig_histogram_2 = px.histogram(df, x="make", color="type", title='Manufacturer Breakdown by Vehicle Type').update_xaxes(categoryorder='total descending')
 
-#fig.show()
 # Display the plot using streamlit
-st.plotly_chart(fig_histogram_2)
 st.title('Histogram-2: Vehicles Sale Days Posted by Condition')
+st.plotly_chart(fig_histogram_2)
+
+
 
 
 
 # DATA VISUALIZATION-Scatter Plot-1: Vehicle Condition by Price
-#df = clean_VEH_df
 fig_scatter_1 = px.scatter(df, x="date_posted", y="price", color="condition", log_y=True,
                  size='days_listed', title='Vehicle Condition by Price', 
                  hover_data=['model_year', 'model_name', 'price', 'type', 'odometer'])
 
-#fig.show()
 # Display the plot using streamlit
-st.plotly_chart(fig_scatter_1)
 st.title('Scatter Plot-1: Vehicle Condition by Price')
+st.plotly_chart(fig_scatter_1)
+
 
 
 
 
 # DATA VISUALIZATION-Scatter Plot-2: Vehicle Sales Price by Milage and Transmission
-#df = clean_VEH_df
 fig_scatter_2 = px.scatter(df, x="odometer", y="price", color="transmission",
                  size='odometer', title='Scatter Plot: Vehicle Sales Price by Milage and Transmission', hover_data=['model_year', 'model_name', 'price', 'type', 'odometer'])
 
-#fig.show()
 # Display the plot using streamlit
-st.plotly_chart(fig_scatter_2)
 st.title('Scatter Plot-2: Vehicle Sales Price by Milage and Transmission')
+st.plotly_chart(fig_scatter_2)
+
 
 
 
 # REFERENCE CODE: for checkbox
-st.checkbox
+#show_histogram = st.checkbox('Show Histogram')
+
+#if show_histogram:
+#    fig = px.histogram(df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
+#    fig.update_layout(yaxis_title="Amount in Stock")
+#    st.plotly_chart(fig)
