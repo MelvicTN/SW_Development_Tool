7d2b4f4f01caad11690af11b7a93cbdeacfa757a
MelvicTN <mtnavida@gmail.com>
2024-07-29 18:26:29 -0700

    Updated sw_dev_app.py: add plotly code for histogram and scatter plots

--- sw_dev_app.py
+++ sw_dev_app.py
@@ -12,6 +12,50 @@ import plotly.express as px
 
 
 # load *.csv file from GitHub repository
+df = pd.read('vehicles_cleaned.csv')
 
 
-vehicles_df = pd.read('cleaned_vehicles_us.csv')
+
+
+
+
+
+# DATA VISUALIZATION: create a histogram using plotly express
+#df = clean_VEH_df
+
+fig = px.histogram(df, x="days_listed", nbins=90, color='condition', title='Histogram Days Listed', hover_data=['model_year', 'make', 'model_name'])
+fig.show()
+
+
+
+
+
+
+# DATA VISUALIZATION: create a histogram using plotly express
+#df = clean_VEH_df
+fig = px.histogram(df, x="make", color="type", title='Histogram: Make Breakdown by Vehicle Type').update_xaxes(categoryorder='total descending')
+fig.show()
+
+
+
+
+
+# DATA VISUALIZATION: create a scatter plot using plotly express
+# Setting size and color with column names
+#df = clean_VEH_df
+fig = px.scatter(df, x="date_posted", y="price", color="condition",
+                 size='days_listed', title='Scatter Plot: Vehicle Condition and Price by Vehicle Condition', hover_data=['model_year', 'model_name', 'price', 'type', 'odometer'])
+fig.show()
+
+
+
+
+
+
+# DATA VISUALIZATION: create a scatter plot using plotly express
+# Setting size and color with column names
+#df = clean_VEH_df
+fig = px.scatter(df, x="odometer", y="price", color="transmission",
+                 size='odometer', title='Scatter Plot: Vehicle Sales Price by Milage and Transmission', hover_data=['model_year', 'model_name', 'price', 'type', 'odometer'])
+
+fig.show()
